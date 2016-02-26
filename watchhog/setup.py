9cda63fda303600ceadd8898d988fced1b197c9c
Pavel Vorobyov <viert@yandex-team.ru>
2016-02-26 14:34:51 +0400

    build fixes

--- watchhog/setup.py
+++ watchhog/setup.py
@@ -11,12 +11,16 @@ if sys.platform == 'darwin':
         # '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/Python.framework/Headers',
         '/usr/local/Cellar/boost/1.56.0/include'
     ]
+elif sys.platform.startswith('linux'):
+    include_dirs = [
+            '/usr/local/include'
+    ]
 
-parser_native = Extension('parser_native',
+parser_native = Extension('hog/parser/parser_native',
                           sources=[parser_source],
-                          include_dirs=include_dirs,
-                          extra_compile_args=['-std=c++11'])
+                          include_dirs=include_dirs)
 setup ( name = 'watchhog',
         version = '1.0',
         description = 'WatchHog is a fast log watcher and parser',
+        packages = find_packages(),
         ext_modules = [parser_native])
